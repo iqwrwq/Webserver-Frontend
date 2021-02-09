896537382a6ac7af7dd6f0b0b3b3b34d052ee862
ather <Arthur.schimpf@gmx.de>
2021-02-09 14:05:33 +0100

    Stable v1.1

---
+++ src/toolbox/partials/createNewDir.php
@@ -0,0 +1,10 @@
+
+      <div class="display-dir-box" id="adding-box">
+         <a class="btn btn-plus" type="menu" id="btn-add"><i id="plu-min" class="fas fa-plus-circle"></i></a>
+         <form method="POST" action="">
+            <div class="input-group">
+               <input type="hidden" id="add-input-field" class="input-field" placeholder="Directory Name" name="directory-name" required />
+               <button type="submit" id="btn-submit" class="btn fas fa-plus-square btn-submit"></a>
+            </div>
+         </form>
+      </div>
--- src/toolbox/partials/main.php
+++ src/toolbox/partials/main.php
@@ -73,14 +73,7 @@ function dir_is_empty($dirname)
             </div>
          <?php endif ?>
       <?php endforeach ?>
-
-      <div class="display-dir-box" id="adding-box">
-         <a class="btn btn-plus" type="menu" id="btn-add"><i id="plu-min" class="fas fa-plus-circle"></i></a>
-         <form method="POST" action="">
-            <div class="input-group">
-               <input type="hidden" id="add-input-field" class="input-field" placeholder="Directory Name" name="directory-name" required />
-               <button type="submit" id="btn-submit" class="btn fas fa-plus-square btn-submit"></a>
-            </div>
-         </form>
-      </div>
+      
+      <?php require_once 'createNewDir.php' ?>
+      
    </div>
